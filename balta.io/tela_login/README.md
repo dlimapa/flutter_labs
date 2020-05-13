49051afa123b135cfa366ad9cd29dbb735a95418
Daniel de Lima <dlima@tre-pa.jus.br>
2020-05-13 00:55:37 -0300

    Balta.io - Login simples - finalizado.

--- balta.io/tela_login/README.md
+++ balta.io/tela_login/README.md
@@ -1,16 +1,21 @@
 # tela_login
 
-A new Flutter project.
+Canal balta.io
 
-## Getting Started
+## Vídeos
 
-This project is a starting point for a Flutter application.
+- Primeiro modelo
 
-A few resources to get you started if this is your first Flutter project:
+https://www.youtube.com/watch?v=Rc_GJpRU6VI
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+- Segundo modelo
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+https://www.youtube.com/watch?v=lmArDnXxvtQ
+
+https://www.youtube.com/watch?v=JSBIg_RsQ34
+
+https://www.youtube.com/watch?v=9gg4sNd9bzw
+
+
+
+
---
+++ balta.io/tela_login/lib/login/login_simples.dart
@@ -0,0 +1,57 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class LoginPageSimples extends StatefulWidget {
+  @override
+  _LoginPageSimplesState createState() => _LoginPageSimplesState();
+}
+
+class _LoginPageSimplesState extends State<LoginPageSimples> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                autofocus: true,
+                keyboardType: TextInputType.number,
+                style: new TextStyle(color: Colors.white, fontSize: 20),
+                decoration: InputDecoration(
+                    labelText: "CPF",
+                    labelStyle: TextStyle(color: Colors.white)),
+              ),
+              Divider(),
+              TextFormField(
+                obscureText: true,
+                keyboardType: TextInputType.text,
+                style: new TextStyle(color: Colors.white, fontSize: 20),
+                decoration: InputDecoration(
+                    labelText: "Senha",
+                    labelStyle: TextStyle(color: Colors.white)),
+              ),
+              Divider(),
+              ButtonTheme(
+                height: 50.0,
+                child: RaisedButton(
+                  onPressed: () => {},
+                  child: Text(
+                    "Entrar",
+                    style: TextStyle(color: Colors.deepPurple),
+                  ),
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
